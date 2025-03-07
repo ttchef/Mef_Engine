d4b680082baef8aadc86a774aff4b745b5735582
ttchef <ruro0.edit@gmail.com>
2025-03-07 20:29:43 +0100

    added glfw as fetch in cmake

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 
+
 mkdir build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=D:/C++Dinge/vcpkg/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_PREFIX_PATH=D:\C++Dinge\vcpkg\vcpkg\installed\x64-windows
+cmake ..
 cmake --build . --config Release
+
 
