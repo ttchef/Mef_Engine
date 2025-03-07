b07b6a320ca0a072732fdeee101a57fcbae29b57
ttchef <ruro0.edit@gmail.com>
2025-03-07 20:48:10 +0100

    added stb_image

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mef_Engine
- 
+
+Im just trying to write a little graphics library in c++ via OpenGL as a little exercise :)
+
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "stb_image.h"
 #include <mef.hpp>
 #include <iostream>
 
