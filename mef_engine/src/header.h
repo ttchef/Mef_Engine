b28013a9643f6cd87c35ae9acdbee8a724c2cc48
ttchef <ruro0.edit@gmail.com>
2025-03-07 18:24:19 +0100

    idk basic folder setup

---
+++ mef_engine/src/header.h
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+class bla
+{
+    public:
+        void pop() 
+        {
+            std::cout << "POP!\n";
+        }
+};
---
+++ project1/src/main.cpp
@@ -0,0 +1,9 @@
+
+#include <header.h>
+
+int main()
+{
+    bla bla;
+    bla.pop();
+    return 0;
+}
